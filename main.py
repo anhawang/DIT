b8cfd58d3a35a1ef8338a466e67e95bce5e23162
anhawang <20awang@wakatipu.school.nz>
2024-03-30 13:24:43 +1300

    day 1

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+class QuizApp:
+    def __init__(self):
+        pass
+
+    # this part im going write func for fetching questions, asking questions, etc
+
+    def main():
+        pass
+
+    if __name__ == "__main__":
+        main()
